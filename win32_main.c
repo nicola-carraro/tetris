dd2bfc4bc4208662f1cc05bdc680a3e40154fe5d
Nicola <carraron985@gmail.com>
2025-11-29 21:25:22 +0100

    Open window

--- win32_main.c
+++ win32_main.c
@@ -1,6 +1,28 @@
 #include <windows.h>
 
+#pragma comment(lib, "User32")
+
 #define TTS_UNREFERENCED(a) a
+
+LRESULT windowProc(
+    HWND window,
+    UINT message,
+    WPARAM wParam,
+    LPARAM lParam
+) {
+    LRESULT result = 0;
+    switch (message) {
+        case WM_DESTROY: {
+            PostQuitMessage(0);
+        } break;
+
+        default: {
+            result = DefWindowProcA(window, message, wParam, lParam);
+        }
+    }
+
+    return result;
+}
 
 int WinMain(
     _In_     HINSTANCE instance,
@@ -8,12 +30,61 @@ int WinMain(
     _In_     LPSTR     commandLine,
     _In_     int       showCommand
 ) {
-    TTS_UNREFERENCED(instance);
     TTS_UNREFERENCED(previousInstance);
     TTS_UNREFERENCED(commandLine);
     TTS_UNREFERENCED(showCommand);
 
-    OutputDebugStringA("Tetris\n");
+    WNDCLASSEXA windowClass = {0};
+    char className[] = "tetris";
+    {
+        windowClass.cbSize = sizeof(windowClass);
+        windowClass.style = 0;
+        windowClass.lpfnWndProc = windowProc;
+        windowClass.cbClsExtra = 0;
+        windowClass.cbWndExtra = 0;
+        windowClass.hInstance = instance;
+        windowClass.hIcon = 0;
+        windowClass.hCursor = LoadCursor(0, IDC_ARROW);
+        windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+        windowClass.lpszMenuName = 0;
+        windowClass.lpszClassName = className;
+        windowClass.hIconSm = 0;
+    }
+
+    if (RegisterClassExA(&windowClass)) {
+        HWND window = CreateWindowExA(
+            WS_EX_NOREDIRECTIONBITMAP,
+            className,
+            "Tetris",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            instance,
+            0
+        );
+
+        if (window) {
+            ShowWindow(window, showCommand);
+
+            MSG message = {0};
+            BOOL ok = 0;
+            while ((ok = GetMessage(&message, 0, 0, 0))) {
+                if (ok == -1) {
+                    break;
+                } else {
+                    TranslateMessage(&message);
+                    DispatchMessageA(&message);
+                }
+            }
+        }
+    }
+
+    DWORD error = GetLastError();
+    TTS_UNREFERENCED(error);
 
     return 0;
 }
